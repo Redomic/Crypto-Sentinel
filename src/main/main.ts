f6d2444db6251eeffc7090cf3bb078e7e6fbef4c
Redomic <redomicmc@gmail.com>
2024-03-10 21:48:48 +0530

    added stuff

--- src/main/main.ts
+++ src/main/main.ts
@@ -127,11 +127,11 @@ app.on('window-all-closed', () => {
 app
   .whenReady()
   .then(() => {
-    createWindow();
+    // createWindow();
     app.on('activate', () => {
       // On macOS it's common to re-create a window in the app when the
       // dock icon is clicked and there are no other windows open.
-      if (mainWindow === null) createWindow();
+      // if (mainWindow === null) createWindow();
     });
   })
   .catch(console.log);
