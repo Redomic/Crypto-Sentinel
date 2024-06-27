de49400ab2ddaedeacd20f440033f4819b041e03
Jones David <redomic@redomic-2.local>
2024-06-28 00:14:25 +0530

    added stuff again

--- src/renderer/agent.ts
+++ src/renderer/agent.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL =
-  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5300/';
+  process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5300/api';
 
 const instance = axios.create({
   baseURL,
@@ -14,7 +14,7 @@ const blockchain = {
   getNodeGraph: (id: string) => instance.get(`/node/${id}/graph`),
   getChat: (id: string) => instance.get(`/chat/${id}`),
   postChat: (message: string, nodeId: string) =>
-    instance.post(`/chat`, { message: message, node: nodeId }),
+    instance.post(`/chat`, { message, node: nodeId }),
   getCrawler: () => instance.get('/crawler'),
 };
 
